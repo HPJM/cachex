73bbd8b8fcb20a9b5c2610ef4a406bd57c124d51
Hugo <hpjm37@gmail.com>
2021-10-12 18:24:55 +0100

    Implement read, write, delete operations

--- lib/cachex.ex
+++ lib/cachex.ex
@@ -1,18 +1,113 @@
 defmodule Cachex do
   @moduledoc """
-  Documentation for `Cachex`.
+  In-memory key-value store for fetching, storing and deleting any type of data.
+
+  Light wrapper around erlang's `ets` cache.
+
+  A `GenServer` owns the table, so you need to add `Cachex` to your supervision tree.
+  `name` is optional, defaults to `Cachex`.
   """
+
+  use GenServer
+
+  @impl true
+  def init(opts) do
+    new_table(opts[:name])
+
+    {:ok, %{}}
+  end
+
+  def start_link(opts) do
+    opts = Keyword.put_new(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: opts[:name])
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cachex.hello()
-      :world
-
+  Removes entry `key` from cache
   """
-  def hello do
-    :world
+  def delete(key, name \\ __MODULE__) do
+    :ets.delete(name, key)
   end
+
+  @doc """
+  Looks up entry `key` in cache.
+
+  Returns `{:error, :expired, insertion_timestamp, ttl}` if `ttl` exceeded
+  and `{:error, :not_found}` if no entry exists.
+
+  Otherwise returns `{:ok, value}`
+  """
+  def fetch(key, name \\ __MODULE__) do
+    {value, timestamp, ttl} = :ets.lookup_element(name, key, 2)
+
+    if expired?(timestamp, ttl) do
+      delete(key, name)
+      {:error, {:expired, value, timestamp, ttl}}
+    else
+      {:ok, value}
+    end
+  rescue
+    ArgumentError -> {:error, :not_found}
+  end
+
+  @doc """
+  Inserts entry `key` in cache with given `value`.
+
+  You can specify a time-to-live (ttl) as an option, defaults to `:infinity`
+  (entry never regarded as expired).
+  """
+  def put(key, value, opts \\ []) do
+    name = Keyword.get(opts, :name, __MODULE__)
+    ttl = Keyword.get(opts, :ttl, :infinity)
+    timestamp = Keyword.get(opts, :timestamp, DateTime.utc_now())
+
+    true = :ets.insert(name, {key, {value, timestamp, ttl}})
+    :ok
+  end
+
+  @doc """
+  If cache entry not found or expired, invokes callback, stores result and returns it in
+  an `{ok, result}` tuple. Callback can be 0- or 1-arity function.
+  If 1-arity, it is passed the key.
+
+  Otherwise returns cached value.
+  """
+  def fetch_or_put(key, fun, opts \\ []) when is_function(fun, 1) or is_function(fun, 0) do
+    name = Keyword.get(opts, :name, __MODULE__)
+
+    case fetch(key, name) do
+      # Entry already in cache
+      {:ok, _} = cached ->
+        cached
+
+      # Lookup has failed
+      {:error, e} when e in [:not_found, :expired] ->
+        value = apply_callback(fun, key)
+        put(key, value, opts)
+        {:ok, value}
+
+      # Fallback
+      error ->
+        error
+    end
+  end
+
+  defp new_table(name) do
+    :ets.new(name, [
+      :set,
+      :named_table,
+      :public,
+      read_concurrency: true,
+      write_concurrency: true
+    ])
+  end
+
+  defp expired?(_timestamp, :infinity), do: false
+
+  defp expired?(timestamp, ttl) do
+    DateTime.compare(DateTime.utc_now(), DateTime.add(timestamp, ttl)) == :gt
+  end
+
+  defp apply_callback(fun, key) when is_function(fun, 1), do: fun.(key)
+  defp apply_callback(fun, _key) when is_function(fun, 0), do: fun.()
 end
--- test/cachex_test.exs
+++ test/cachex_test.exs
@@ -2,7 +2,59 @@ defmodule CachexTest do
   use ExUnit.Case
   doctest Cachex
 
-  test "greets the world" do
-    assert Cachex.hello() == :world
+  # Uniquely name cache each test so we don't have to look up PID
+  setup context do
+    start_supervised!({Cachex, name: context.test})
+    %{name: context.test}
+  end
+
+  describe "put" do
+    test "put/3 inserts entry into cache", %{name: name} do
+      Cachex.put(:key, :value, ttl: 5, name: name)
+      assert {value, _timestamp, ttl} = :ets.lookup_element(name, :key, 2)
+      assert value == :value
+      assert ttl == 5
+    end
+  end
+
+  describe "fetch" do
+    test "fetch/2 fetches entry if exists", %{name: name} do
+      :ets.insert(name, {:another_key, {:another_value, DateTime.utc_now(), 5}})
+      assert Cachex.fetch(:another_key, name) == {:ok, :another_value}
+    end
+
+    test "fetch/2 returns errors and time metadata on expiry", %{name: name} do
+      now = DateTime.utc_now()
+      Cachex.put(:blue, :cheese, ttl: 0, name: name, timestamp: now)
+      assert Cachex.fetch(:blue, name) == {:error, {:expired, :cheese, now, 0}}
+      # Check deleted
+      assert Cachex.fetch(:blue, name) == {:error, :not_found}
+    end
+
+    test "fetch/2 returns error when not found", %{name: name} do
+      assert Cachex.fetch(:wensleydale, name) == {:error, :not_found}
+    end
+  end
+
+  describe "delete" do
+    test "delete/2 removes entry from cache", %{name: name} do
+      Cachex.put(:red, :leicester, name: name)
+      assert Cachex.delete(:red, name)
+      assert Cachex.fetch(:red, name) == {:error, :not_found}
+    end
+  end
+
+  describe "fetch_or_put" do
+    test "fetch_or_put/3 puts if doesn't exist", %{name: name} do
+      assert Cachex.fetch_or_put(:number, &"#{&1}", name: name) == {:ok, "number"}
+
+      assert Cachex.fetch(:number, name) == {:ok, "number"}
+    end
+
+    test "fetch_or_put/3 fetches if exists", %{name: name} do
+      Cachex.put(:key, :value, name: name)
+
+      assert Cachex.fetch_or_put(:key, fn -> 1 end, name: name) == {:ok, :value}
+    end
   end
 end
